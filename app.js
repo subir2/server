25f098beeb30424525a3ead6077bb9e65b13c95f
subir2 <subirsarker1995@gmail.com>
2022-05-06 10:48:35 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const errorMiddleware = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const app=express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
+
+app.use(errorMiddleware);
+
+
+module.exports=app;
